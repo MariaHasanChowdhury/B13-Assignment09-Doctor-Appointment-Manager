9c7b8b1909e01aedce45541a0f0a39301954e4ab
MariaHasanChowdhury <mariahasanchowdhury1130@gmail.com>
2026-06-07 12:00:38 +0600

    add data in routes folder in doctor routes

--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Doctor = require("../models/Doctor");
 
-// GET all doctors
 router.get("/", async (req, res) => {
   const doctors = await Doctor.find();
   res.json(doctors);
+});
+
+router.post("/", async (req, res) => {
+  const doctor = await Doctor.create(req.body);
+  res.json(doctor);
 });
 
 module.exports = router;
