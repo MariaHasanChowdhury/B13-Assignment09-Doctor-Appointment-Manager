ce9ba4fc1088a53c60ce9c187dc60799334a6780
MariaHasanChowdhury <mariahasanchowdhury1130@gmail.com>
2026-06-10 08:13:51 +0600

    create appoiment routes

---
+++ server/routes/appoinmentRoutes.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+const Appointment = require("../models/Appointment");
+
+// Create Appointment
+router.post("/", async (req, res) => {
+  try {
+    const appointment =
+      await Appointment.create(req.body);
+
+    res.status(201).json(appointment);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+// Get All Appointments
+router.get("/", async (req, res) => {
+  try {
+    const appointments =
+      await Appointment.find()
+        .populate("doctorId");
+
+    res.json(appointments);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
