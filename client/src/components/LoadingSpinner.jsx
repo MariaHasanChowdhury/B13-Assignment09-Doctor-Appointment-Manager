98cbd542b2e52aeb97850f1b93da3306ab702d93
MariaHasanChowdhury <mariahasanchowdhury1130@gmail.com>
2026-06-19 11:37:27 +0600

    create loading spinner

---
+++ client/src/components/LoadingSpinner.jsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="flex justify-center items-center min-h-[50vh]">
+      <span className="loading loading-spinner loading-lg text-primary"></span>
+    </div>
+  );
+}
